6cf11494fa3c899520bef46a28fa46e8309c9159
Paul Peacock <pcp1976@gmail>
2018-12-30 13:06:38 +0000

    Move settings variables out of app

--- app/component/app.py
+++ app/component/app.py
@@ -1,66 +1,17 @@
-
+from app.component.settings import \
+    log, \
+    EVENT_STORE_URL, \
+    EVENT_STORE_HTTP_PORT, \
+    EVENT_STORE_TCP_PORT, \
+    EVENT_STORE_USER, \
+    EVENT_STORE_PASS
 import asyncio
 from photonpump import connect, exceptions
 import json
-import os
-import logging
 import functools
-from configobj import ConfigObj
 import uuid
 import requests
 import pyaml
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-CONFIG = ConfigObj(os.path.join(dir_path, "config.ini"))
-ENVIRON = os.getenv("ENVIRON", CONFIG["config"]["ENVIRON"])
-
-EVENT_STORE_URL = os.getenv("EVENT_STORE_URL", CONFIG[ENVIRON]["EVENT_STORE_URL"])
-EVENT_STORE_HTTP_PORT = int(os.getenv("EVENT_STORE_HTTP_PORT", CONFIG[ENVIRON]["EVENT_STORE_HTTP_PORT"]))
-EVENT_STORE_TCP_PORT = int(os.getenv("EVENT_STORE_TCP_PORT", CONFIG[ENVIRON]["EVENT_STORE_TCP_PORT"]))
-EVENT_STORE_USER = os.getenv("EVENT_STORE_USER", CONFIG[ENVIRON]["EVENT_STORE_USER"])
-EVENT_STORE_PASS = os.getenv("EVENT_STORE_PASS", CONFIG[ENVIRON]["EVENT_STORE_PASS"])
-
-LOGGER_LEVEL = int(os.getenv("LOGGER_LEVEL", CONFIG[ENVIRON]["LOGGER_LEVEL"]))
-LOGGER_FORMAT = '%(asctime)s [%(name)s] %(message)s'
-
-V_MA = CONFIG["version"]["MAJOR"]
-V_MI = CONFIG["version"]["MINOR"]
-V_RE = CONFIG["version"]["REVISION"]
-V_DATE = CONFIG["version"]["DATE"]
-CODENAME = CONFIG["version"]["CODENAME"]
-
-logging.basicConfig(format=LOGGER_FORMAT, datefmt='[%H:%M:%S]')
-log = logging.getLogger("spine")
-
-"""
-CRITICAL 50
-ERROR    40
-WARNING  30
-INFO     20
-DEBUG    10
-NOTSET    0
-"""
-log.setLevel(LOGGER_LEVEL)
-
-
-def version_fancy():
-    return ''.join((
-        "\n",
-        " (  (                       (         (           )", "\n",
-        " )\))(   ' (   (  (  (  (   )\ (    ( )\       ( /(", "\n",
-        "((_)()\ )  )\  )\))( )\))( ((_))\ ) )((_)  (   )\())", "\n",
-        "_(())\_)()((_)((_))\((_))\  _ (()/(((_)_   )\ (_))/", "\n",
-        "\ \((_)/ / (_) (()(_)(()(_)| | )(_))| _ ) ((_)| |_ ",
-        "         version: {0}".format("v%s.%s.%s" % (V_MA, V_MI, V_RE)), "\n",
-        " \ \/\/ /  | |/ _` |/ _` | | || || || _ \/ _ \|  _|",
-        "       code name: {0}".format(CODENAME), "\n",
-        "  \_/\_/   |_|\__, |\__, | |_| \_, ||___/\___/ \__|",
-        "    release date: {0}".format(V_DATE), "\n",
-        "              |___/ |___/      |__/", "\n"
-    ))
-
-
-log.info(version_fancy())
 
 
 def run_in_executor(f):
