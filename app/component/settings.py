87dcb86d1709ee54f14b21a3ecbe1916b7436f09
Paul Peacock <pcp1976@gmail>
2019-01-03 21:23:43 +0000

    Use component name in settings for logger

--- app/component/settings.py
+++ app/component/settings.py
@@ -1,6 +1,8 @@
 import os
 from configobj import ConfigObj
 import logging
+import urllib.parse
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 CONFIG = ConfigObj(os.path.join(dir_path, "config.ini"))
@@ -12,6 +14,11 @@ EVENT_STORE_TCP_PORT = int(os.getenv("EVENT_STORE_TCP_PORT", CONFIG[ENVIRON]["EV
 EVENT_STORE_USER = os.getenv("EVENT_STORE_USER", CONFIG[ENVIRON]["EVENT_STORE_USER"])
 EVENT_STORE_PASS = os.getenv("EVENT_STORE_PASS", CONFIG[ENVIRON]["EVENT_STORE_PASS"])
 
+MONGO_URL = os.getenv("MONGO_URL", CONFIG[ENVIRON]["MONGO_URL"])
+MONGO_PORT = int(os.getenv("MONGO_PORT", CONFIG[ENVIRON]["MONGO_PORT"]))
+MONGO_USER = urllib.parse.quote_plus(os.getenv("MONGO_USER", CONFIG[ENVIRON]["MONGO_USER"]))
+MONGO_PASS = urllib.parse.quote_plus(os.getenv("MONGO_PASS", CONFIG[ENVIRON]["MONGO_PASS"]))
+
 LOGGER_LEVEL = int(os.getenv("LOGGER_LEVEL", CONFIG[ENVIRON]["LOGGER_LEVEL"]))
 LOGGER_FORMAT = '%(asctime)s [%(name)s] %(message)s'
 
@@ -22,8 +29,7 @@ V_DATE = CONFIG["version"]["DATE"]
 CODENAME = CONFIG["version"]["CODENAME"]
 
 logging.basicConfig(format=LOGGER_FORMAT, datefmt='[%H:%M:%S]')
-
-log = logging.getLogger("spine")
+log = logging.getLogger(CONFIG["config"]["NAME"])
 
 """
 CRITICAL 50
